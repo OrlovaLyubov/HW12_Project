b7c66a60b2f31dee152135b09946274260eca286
OrlovaLyubov <lyubko.orlova@yandex.ru>
2024-06-19 21:25:34 +0300

    добавлено название параметра в разделе Настройки

--- HW8_UIViewController_OrlovaLG/SettingsViewController.swift
+++ HW8_UIViewController_OrlovaLG/SettingsViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var changeThemeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor(red: 219/255, green: 212/255, blue: 250/255, alpha: 1)
+        
+        changeThemeLabel.text = "Изменить тему"
     }
 }
