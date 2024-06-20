365caecd87f50f28e679c1f52fe341881d5ee445
OrlovaLyubov <lyubko.orlova@yandex.ru>
2024-06-20 17:39:00 +0300

    Добавлены названия настроек

--- HW8_UIViewController_OrlovaLG/SettingsViewController.swift
+++ HW8_UIViewController_OrlovaLG/SettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var changeThemeLabel: UILabel!
+    @IBOutlet weak var supportLabel: UILabel!
+    @IBOutlet weak var versionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,7 @@ class SettingsViewController: UIViewController {
         view.backgroundColor = UIColor(red: 219/255, green: 212/255, blue: 250/255, alpha: 1)
         
         changeThemeLabel.text = "Изменить тему"
+        supportLabel.text = "Написать в поддержку"
+        versionLabel.text = "Версия приложения"
     }
 }
